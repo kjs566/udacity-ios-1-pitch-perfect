f669b241ce35977aada6f264141e1256cd869143
Jan Skála <jan.skala566@gmail.com>
2018-03-08 15:26:56 +0100

    Created home screen

--- PitchPerfect/PitchPerfect/HomeViewController.swift
+++ PitchPerfect/PitchPerfect/HomeViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var recordStopButton: UIButton!
+    
+    @IBOutlet weak var hintLabel: UILabel!
+    
+    static let hintRecordVoice = "Record your voice first and enjoy it's funny modifications";
+    static let hintStopRecording = "Stop recording and start modifying your voice";
+    
+    enum RecordingState{
+        case recording, not_recording
+    }
+    
+    var currentState = RecordingState.not_recording
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    /// Called before showing views, setting initial helping instruction.
+    ///
+    /// - Parameter animated:
+    override func viewWillAppear(_ animated: Bool) {
+        updateView();
     }
 
-
+    
+    /// Called whan recording control button is clicked
+    ///
+    /// - Parameter sender: 
+    @IBAction func recordStopClicked(_ sender: Any) {
+        switch(currentState){
+            case .recording:
+                currentState = .not_recording
+            case .not_recording:
+                currentState = .recording
+                stopRecording();
+        }
+        updateView()
+    }
+    
+    
+    /// Updates recording control button and hint label
+    func updateView(){
+        var buttonImage : UIImage!
+        switch(currentState){
+            case .recording:
+                setHint(HomeViewController.hintStopRecording)
+                buttonImage = #imageLiteral(resourceName: "Stop")
+            default:
+                setHint(HomeViewController.hintRecordVoice)
+                buttonImage = #imageLiteral(resourceName: "Record")
+        }
+        setButtonImage(buttonImage)
+    }
+    
+    
+    func setHint(_ hint : String){
+        hintLabel?.text = hint
+    }
+    
+    func setButtonImage(_ image : UIImage){
+        recordStopButton.setImage(image , for: .normal)
+    }
+    
+    func stopRecording(){
+        
+    }
 }
 
